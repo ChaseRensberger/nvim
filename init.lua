8e7e2b01a4f083038767a08135678728514c7232
ChaseRensberger <chasehrensberger@gmail.com>
2024-09-23 15:17:31 -0400

    formatter installed

--- init.lua
+++ init.lua
@@ -10,86 +10,92 @@ vim.opt.relativenumber = true
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  spec = {
-	  {
-		  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		  dependencies = { 'nvim-lua/plenary.nvim' }
-	  },
-	  { "rose-pine/neovim", name = "rose-pine" },
-	  {
-		  'nvim-treesitter/nvim-treesitter',
-		  build = ':TSUpdate'
-	  },
-	  { 'github/copilot.vim' },
-	  {
-		  "williamboman/mason.nvim",
-		  "williamboman/mason-lspconfig.nvim",
-		  "neovim/nvim-lspconfig",
-	  },
-	  -- { 'hrsh7th/nvim-cmp' },
-	  -- { 'hrsh7th/cmp-nvim-lsp' },
-	  -- { 'hrsh7th/cmp-buffer' },
-	  -- { 'hrsh7th/cmp-path' },
-	  -- { 'hrsh7th/cmp-cmdline' },
-  },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	spec = {
+		{
+			"nvim-telescope/telescope.nvim",
+			tag = "0.1.8",
+			dependencies = { "nvim-lua/plenary.nvim" },
+		},
+		{ "rose-pine/neovim", name = "rose-pine" },
+		{
+			"nvim-treesitter/nvim-treesitter",
+			build = ":TSUpdate",
+		},
+		{ "github/copilot.vim" },
+		{
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		{
+			"stevearc/conform.nvim",
+			opts = {},
+		},
+		-- { 'hrsh7th/nvim-cmp' },
+		-- { 'hrsh7th/cmp-nvim-lsp' },
+		-- { 'hrsh7th/cmp-buffer' },
+		-- { 'hrsh7th/cmp-path' },
+		-- { 'hrsh7th/cmp-cmdline' },
+	},
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
 vim.cmd("colorscheme rose-pine")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-require("nvim-treesitter.configs").setup {
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "lua" },
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+	ensure_installed = { "lua" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
 
-  highlight = {
-    enable = true,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-}
-
+	highlight = {
+		enable = true,
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+})
 
 require("mason").setup()
-require("mason-lspconfig").setup {
-	ensure_installed = { "lua_ls" }
-}
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls" },
+})
 
-servers = { "lua-language-server" }
+require("lspconfig")["lua_ls"].setup({})
 
-for _, server in ipairs(servers) do
-	require("lspconfig")[server].setup{}
-end
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+	},
+})
